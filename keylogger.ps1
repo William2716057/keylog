a7e3d48888730bf063eb44f78d1c7366515abb58
User <Willspri@gmail.com>
2025-03-02 16:33:48 +1300

    removed brackets

--- keylogger.ps1
+++ keylogger.ps1
@@ -10,7 +10,7 @@ Write-Host "Keylogger started. Press ESC to stop."
 while ($true) {
     $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
     if ($key -eq 27) { break }
-    Add-Content -Path $logFile -Value "($key)"
+    Add-Content -Path $logFile -Value "$key"
 }
 
-Write-Host "Keylogger stopped."
+#Write-Host "Keylogger stopped."
